b3d45f4324a96f1efa44935cb9244dbc4c93dea1
Pariya Jafari <56969706+pariyaj@users.noreply.github.com>
2024-08-25 20:15:19 +0330

    Create WebConfig.java

---
+++ S03/Request Mapping/WebConfig.java
@@ -0,0 +1,10 @@
+    @Bean    
+    View registerformNew(){
+        return new JstlView("/WEB-INF/view6/registerformNew.jsp");
+    }
+
+    
+    @Bean    
+    View result1(){
+        return new JstlView("/WEB-INF/view6/result1.jsp");
+    }
