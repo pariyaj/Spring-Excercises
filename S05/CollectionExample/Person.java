342f3d1a01ae74bcee6c7dd4133cf57a1b4a0436
Pariya Jafari <56969706+pariyaj@users.noreply.github.com>
2024-09-07 00:30:07 +0330

    Update Person.java

--- S05/CollectionExample/Person.java
+++ S05/CollectionExample/Person.java
@@ -6,46 +6,35 @@ public class Person implements Comparable<Person>{
     private String address
     
     public Person(String name, String fname, int age, String address) {
-        this.name = name;
-        this.fname = fname;
-        this.age = age;
-        this.address = address;
+	    this.name = name;
+	    this.fname = fname;
+	    this.age = age;
+	    this.address = address;
     }
-
-   public String getName() {
-        return name;
-    }
-
-   public String getFname() {
-        return fname;
-    }
-
-   public int getAge() {
-      return age;
-    }
-
-   public String getAddress() {
-      return adress;
-    }
-    
-   public void setName(String name) {
-    	this.name = name;
-		
+	public String getName() {
+		return name;
 	}
-
+	public String getFname() {
+		return fname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public String getAddress() {
+		return adress;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	public void setFname(String fname) {
 		this.fname = fname;
 	}
-
 	public void setAge(int age) {
 		this.age = age;
 	}
-
-  public void setAdress(String address) {
-    this.address = address;
-		
+	public void setAdress(String address) {
+		this.address = address;
 	}
-
 	@Override
 	public int compareTo(Person o) {
 		return this.age - o.age;
