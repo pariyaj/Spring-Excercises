19b4430b471d229f58582b6aa5ce09811dc6761c
Pariya Jafari <56969706+pariyaj@users.noreply.github.com>
2024-09-07 00:36:31 +0330

    Create Address.java

---
+++ S05/CollectionExample/Address.java
@@ -0,0 +1,21 @@
+public class Adress {
+    	
+    private String city;
+    private String country;
+    
+    public Address(String city, String country) {
+	    this.city = city;
+	    this.country = country;
+    }
+  public String getCity() {
+		return city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
