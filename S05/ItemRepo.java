ddb6315cad293a04c04ce84b3be43f6b793d7ba5
Pariya Jafari <56969706+pariyaj@users.noreply.github.com>
2024-09-06 23:55:26 +0330

    Update ItemRepo.java

--- S05/ItemRepo.java
+++ S05/ItemRepo.java
@@ -3,5 +3,5 @@ public interface ItemJpaRepository extends CrudRepository<Item, Integer> {
 	List<Item> findByCategoryName(String name);
 	List<Item> findByNameStartingWith(String name);	
 	List<Item> findByCategoryLocationNameStartingWith(String name);
-  List<Item> findByNameIncluding(String name);
+        List<Item> findByNameIncluding(String name);
 }
