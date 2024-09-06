822f5d4c1681d1f19ef21b8041532b9c540a8742
Pariya Jafari <56969706+pariyaj@users.noreply.github.com>
2024-09-06 23:37:03 +0330

    Create Category.java

---
+++ S05/Category.java
@@ -0,0 +1,39 @@
+@Entity
+public class Category {
+	
+	@Id
+	private long id;	
+	private String name;
+  @OneToOne(mappedBy = "category_id")
+	private String location;
+	
+	public Category() {};
+	
+	public Category(long id, String name, String location) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.location = location;
+	}
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
