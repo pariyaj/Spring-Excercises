53b746831f35913f4a2b906be6d5e68a8ca3618e
Pariya Jafari <56969706+pariyaj@users.noreply.github.com>
2024-09-07 01:14:29 +0330

    Update collection.java

--- S05/CollectionExample/collection.java
+++ S05/CollectionExample/collection.java
@@ -1,10 +1,9 @@
 public class collection {
 	public static void main(String[] args) {
-
 		Person mehdi = new Person("Mehdi", "Mohammadi", 18, "12 St NewYork"); 
 		Person pariya = new Person("Pariya", "Jafari", 29, "12 St Shiraz");
-		Person ali = new Person("Ali", "Rostami", 20, "12 St Tehran"); 
-    Person sara = new Person("Sara", "Ahmadi", 25, "Shiraz");
+		Person ali = new Person("Ali", "Rostami", 20, "12 St Tehran");
+		Person sara = new Person("Sara", "Ahmadi", 25, "Shiraz");
 		Person saman = new Person("Saman", "Razavi", 21, "12 St Dalas"); 
 		Person aram = new Person("Aram", "Mousavi", 14, "12 St Texas"); 
 		Person arezou = new Person("Arezou", "Mohammadi", 30, "12St Washington DC"); 
@@ -14,17 +13,13 @@ public class collection {
 
 		Person[] presonsArray = {mehdi,pariya,ali,sara,saman,aram,arezou,nima,sarina,sina};
 		List persons = Arrays.asList(presonsArray);
-    
-    persons.size();
-    
-    Collections.sort( persons );
-    System.out.println( persons );
-    
-    public class SortPersonByAge implements Comparator<Person> {
-      @Override
-      public int compare(Person p1, Person p2) {
-        return p1.getAge() - p2.getAge();
-      }
-    }
-    
-  }
+		persons.size();
+		Collections.sort( persons );
+		System.out.println( persons );
+		public class SortPersonByAge implements Comparator<Person> {
+			@Override
+			public int compare(Person p1, Person p2) {
+				return p1.getAge() - p2.getAge();
+			}
+		}
+	}
